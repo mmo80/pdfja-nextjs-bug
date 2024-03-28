0d7b68bfb60932ae7988c191dea0dea4971fec54
Miguel Mendes <miguel.mendes.o@gmail.com>
2024-03-28 15:34:18 +0100

    remove next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    esmExternals: true,
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.node/,
-      use: 'raw-loader',
-    });
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
